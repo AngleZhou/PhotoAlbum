24ad916869f36e3f9f65beac93534b5e95c68b03
ZhouQian <zhouq87724@163.com>
2016-08-23 16:07:08 +0800

    add demo, and documentation

--- Example/Classes/Controller/PhotoAlbums.h
+++ Example/Classes/Controller/PhotoAlbums.h
@@ -19,19 +19,48 @@
 
 
 @interface PhotoAlbums : NSObject
-
+/**
+ *  designated initializer，更多参数可配
+ *
+ *  @param maxImagesCount 最大张数
+ *  @param type           相册类型
+ *  @param bSingleSelect  是否单选
+ *  @param bEnableCrop    是否剪裁
+ *  @param delegate       <#delegate description#>
+ *  @param finishBlock    图片选择回调
+ */
 + (void)photoWithMaxImagesCount:(NSInteger)maxImagesCount type:(ZQAlbumType)type bSingleSelect:(BOOL)bSingleSelect crop:(BOOL)bEnableCrop delegate:(id)delegate didFinishPhotoBlock:(void (^)(NSArray<UIImage*> *photos))finishBlock;
 
 
-//single select
+/**
+ *  单选照片
+ *
+ *  @param crop        是否支持剪裁
+ *  @param delegate    <#delegate description#>
+ *  @param finishBlock 选择后返回UIImage数组，只有一个UIImage
+ */
 + (void)photoSingleSelectWithCrop:(BOOL)crop delegate:(id)delegate didFinishPhotoBlock:(void (^)(NSArray<UIImage*> *photos))finishBlock;
 
-//multi-select
+
+/**
+ *  多选照片
+ *
+ *  @param maxImagesCount 最多可选的张数
+ *  @param delegate       <#delegate description#>
+ *  @param finishBlock    选择后返回UIImage数组
+ */
 + (void)photoMultiSelectWithMaxImagesCount:(NSInteger)maxImagesCount delegate:(id)delegate didFinishPhotoBlock:(void (^)(NSArray<UIImage*> *photos))finishBlock;
 
 
 
-//video
+/**
+ *  单选视频（视频默认单选）
+ *
+ *  @param duration                    最大支持的视频时长
+ *  @param delegate                    <#delegate description#>
+ *  @param uiUpdateBlock               选择视频后关掉相册前更新需要显示的界面
+ *  @param didFinishPickingVideoHandle 返回视频的本地地址，封面（第一帧），和视频资源
+ */
 + (void)photoVideoWithMaxDurtion:(NSTimeInterval)duration
                         Delegate:(id)delegate
       updateUIFinishPickingBlock:(void (^)(UIImage *cover))uiUpdateBlock
--- Example/PhotoAlbum/ZQViewController.m
+++ Example/PhotoAlbum/ZQViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ZQViewController.h"
+#import "PhotoAlbums.h"
 
 @interface ZQViewController ()
 
@@ -20,6 +21,24 @@
 	
 }
 
+- (IBAction)singleSelectPhoto:(id)sender {
+    [PhotoAlbums photoSingleSelectWithCrop:NO delegate:self didFinishPhotoBlock:^(NSArray<UIImage *> *photos) {
+        NSLog(@"%@", [photos firstObject]);
+    }];
+}
+- (IBAction)multiSelectPhoto:(id)sender {
+    [PhotoAlbums photoMultiSelectWithMaxImagesCount:9 delegate:self didFinishPhotoBlock:^(NSArray<UIImage *> *photos) {
+        NSLog(@"%@", photos);
+    }];
+}
+
+- (IBAction)video:(id)sender {
+    [PhotoAlbums photoVideoWithMaxDurtion:3 Delegate:self updateUIFinishPickingBlock:nil didFinishPickingVideoHandle:^(NSURL *url, UIImage *cover, id avAsset) {
+        NSLog(@"%@", url);
+        NSLog(@"%@", cover);
+        NSLog(@"%@", avAsset);
+    }];
+}
 
 
 @end
