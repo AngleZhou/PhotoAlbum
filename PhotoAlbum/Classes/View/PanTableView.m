ad3ba6d9ccc57bb971ec51cc45a34d8e6e297305
shengjiehou123 <945947485@qq.com>
2019-06-12 22:26:50 +0800

    fix bug

---
+++ PhotoAlbum/Classes/View/PanTableView.m
@@ -0,0 +1,31 @@
+//
+//  PanTableView.m
+//  dope
+//
+//  Created by 盛杰厚 on 2019/6/8.
+//  Copyright © 2019 Dope. All rights reserved.
+//
+
+#import "PanTableView.h"
+
+@implementation PanTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer{
+    if ([gestureRecognizer isKindOfClass:NSClassFromString(@"UIPanGestureRecognizer")]) {
+        CGPoint velocity = [(UIPanGestureRecognizer *)gestureRecognizer velocityInView:gestureRecognizer.view];
+        if (self.contentOffset.y >= 0 && velocity.y > 0) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
+
+@end
